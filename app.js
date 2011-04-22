c94e92b2877ed27ad6b72d704866db8a96e7b2b5
Samyak Bhuta <samyak.bhuta@gmail.com>
2011-04-22 17:32:43 +0530

    Now support for server:1234/update/location/activitmessage/

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require("express");
 var httpServer = express.createServer();
 var nowjs=require("now");
 var everyone = nowjs.initialize(httpServer);
+var geo = require("geo");
 
 //everyone.connected();
 
@@ -25,11 +26,6 @@ var everyone = nowjs.initialize(httpServer);
 
 httpServer.configure(function(){
 	httpServer.use(express.logger());
-//	httpServer.set('views', __dirname + '/views');
-//	httpServer.set('view engine','jade');
-//	httpServer.set('view options', {
-//	  layout: true
-//	});
 });
 httpServer.configure("development",function(){
 	httpServer.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
@@ -41,15 +37,54 @@ httpServer.configure("production",function(){
 });
 
 
+dummyCoords = "22_18_00_N_73_12_01_E";
+dummyTimestamp = "123456";
+dummyPosition = "Vadodara";
+dummyPosition.type="City";
+dummyPosition.activity="Hacking around node.js";
+dummyPosition.coords = dummyCoords;
+dummyPosition.timestamp = dummyTimestamp;
+
+var setToGoogleGeoedLocation = function (formattedAddress,lat,lng){
+	console.log("1 "+formattedAddress);
+	console.log("2 "+lat);
+	console.log("3 "+lng);
+};
+
+
+/*
+As told in now.js documentation, it is better to share the data with the help of fuction.
+So we would not like something like this. Thought it seems obvious to do.
+everyone.now.currentPosition = dummyPosition;
+For every update a client is notified by calling function updateLocation();
+*/
+
+
+/*
+	Just kept for quick easy testing ...
+	Example : example.com:1234/update
+	Should use dummy/hard coded values.
+*/
+
 httpServer.get("/update",function(req,res){
-	position = {};
-	everyone.now.updateLocation({lng:"0.1",lat:"0.1"});
+	everyone.now.updateLocation(dummyPosition);
+	res.end();
+});
+
+/*
+	Example : example.com:1234/update/vadodara/hacking the node.js
+	Based on raw location name string we need to find out rest of the position details with geo code lookup.
+	We use "geo" module for the same.		
+*/
+httpServer.get("/update/:locationName/:activityMessage",function(req,res){	
+	var address = req.params.locationName;
+	var sensor = false;
+	geo.geocoder = (geo.google,address,sensor,setToGoogleGeoedLocation);
+	everyone.now.updateLocation(req.params.locationName,req.params.activityMessage);
 	res.end();
 });
 
 httpServer.listen(1234);
-
-
 console.log("listening at localhost:1234");
 
 
